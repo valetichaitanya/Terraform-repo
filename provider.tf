5097395f31e7279a78be7a3294f800dc3567fe65
valeti chaitanya <cval2078@gmail.com>
2021-10-04 18:56:48 -0400

    last

--- provider.tf
+++ provider.tf
@@ -2,7 +2,8 @@ provider "aws" {
 
   region = "us-east-1"
 
-  access_key = "AKIAS2E46PH2WHJMJLFE"
-  secret_key = "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"
+  access_key = "AKIAS2E46PH2RQXD5M66"
+
+  secret_key = "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"
    
 }
