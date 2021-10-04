daed867255c223900ac031da20ea8bccf14f5124
valeti chaitanya <cval2078@gmail.com>
2021-10-04 18:50:41 -0400

    updated

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
 
   region = "us-east-1"
-  
-  access_key = "AKIAS2E46PH2SD7JT2PG"
-  secret_key = "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"
+
+  access_key = "AKIAS2E46PH2WHJMJLFE"
+  secret_key = "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"
    
 }
